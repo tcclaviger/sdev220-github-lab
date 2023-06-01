6ddbae65a646472e4888840e2669b9d8f28b7e7b
Clav <tcclaviger@gmail.com>
2023-06-01 05:02:33 -0700

    Added documentation to the project.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,13 @@
+/**
+ * This is the entry point to my sample app.
+ * @author rob
+ * @version 1.0
+ */
 public class Main {
+    /**
+     * Stars the program
+     * @param args command-line args (not used)
+     */
     public static void main(String[] args) {
         System.out.println("Hello world, from SDEV 220!");
     }
